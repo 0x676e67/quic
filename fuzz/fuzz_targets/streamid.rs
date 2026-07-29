671ad39bfd43609453dc3230df0d26300d251c84
gngpp <gngppz@gmail.com>
2026-07-29 14:15:31 +0800

    ci: make fuzz targets compatible with workspace checks

--- fuzz/fuzz_targets/streamid.rs
+++ fuzz/fuzz_targets/streamid.rs
@@ -1,19 +1,26 @@
-#![no_main]
-use arbitrary::Arbitrary;
-use libfuzzer_sys::fuzz_target;
+#![cfg_attr(fuzzing, no_main)]
 
-extern crate proto;
-use proto::{Dir, Side, StreamId};
+#[cfg(not(fuzzing))]
+fn main() {}
 
-#[derive(Arbitrary, Debug)]
-struct StreamIdParams {
-    side: Side,
-    dir: Dir,
-    index: u64,
+#[cfg(fuzzing)]
+mod target {
+    use arbitrary::Arbitrary;
+    use libfuzzer_sys::fuzz_target;
+
+    extern crate proto;
+    use proto::{Dir, Side, StreamId};
+
+    #[derive(Arbitrary, Debug)]
+    struct StreamIdParams {
+        side: Side,
+        dir: Dir,
+        index: u64,
+    }
+
+    fuzz_target!(|data: StreamIdParams| {
+        let s = StreamId::new(data.side, data.dir, data.index);
+        assert_eq!(s.initiator(), data.side);
+        assert_eq!(s.dir(), data.dir);
+    });
 }
-
-fuzz_target!(|data: StreamIdParams| {
-    let s = StreamId::new(data.side, data.dir, data.index);
-    assert_eq!(s.initiator(), data.side);
-    assert_eq!(s.dir(), data.dir);
-});
